02bd658c4a39ad3a729360c5d0df45b631e4dd84
Denis <denis.tolmachev@gmail.com>
2023-01-12 20:43:20 +0200

    added mock products

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,12 @@
+import { products } from '../../mock/products';
 import { ProductCard } from './ProductCard/ProductCard';
 import { SearchBar } from './SearchBar/SearchBar';
 
+console.log(products);
+
 export const Content = () => {
   return (
-    <div className='p-40 m-auto'>
+    <div className='p-10 m-auto'>
       <div className='flex flex-col sm:flex-row align-center justify-between'>
         <div>
           <h1 className='text-3xl font-bold'>All sneakers</h1>
@@ -11,7 +14,7 @@ export const Content = () => {
         <SearchBar />
       </div>
 
-      <div className='flex flex-wrap gap-5 lg:gap-9 mt-40'>
+      <div className='flex flex-wrap gap-5 lg:gap-9 mt-10'>
         <ProductCard />
         <ProductCard />
         <ProductCard />
---
+++ src/mock/products.js
@@ -0,0 +1,58 @@
+import { nanoid } from 'nanoid';
+
+export const products = [
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike Blazer Mid Suede',
+    price: '300',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike Air Max 270',
+    price: '210',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike Blazer Mid Suede',
+    price: '300',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Puma X Aka Boku Future Rider',
+    price: '320',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Under Armour Curry 8',
+    price: '402',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike Kyrie 7',
+    price: '351',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Jordan Air Jordan 11',
+    price: '500',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike LeBron XVIII',
+    price: '236',
+    img: '',
+  },
+  {
+    id: nanoid(),
+    name: 'Men`s sneakers Nike Lebron XVIII Low',
+    price: '235',
+    img: '',
+  },
+];
