25fe6bfa3f54b2195cce7e2166409547f584d374
Denis <denis.tolmachev@gmail.com>
2023-02-07 21:17:23 +0200

    added orders list

---
+++ src/components/Orders/OrdersItems.jsx
@@ -0,0 +1,58 @@
+import emptyOrders from '../../img/emptyOrders.png';
+import { InfoPage } from '../InfoPage/InfoPage';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export const OrdersItems = () => {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const orderResponse = await axios.get('/orders');
+      setOrders(orderResponse.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      {orders.length > 0 ? (
+        <div className='flex flex-col-reverse'>
+          {orders.map(order => (
+            <div className='border-b-2 first:border-b-0' key={order.id}>
+              <div className='py-5'>
+                <h3 className='text-xl font-bold'>Order #{order.id}</h3>
+                <div className='flex flex-wrap gap-5 mt-2'>
+                  {order.products.map(product => (
+                    <div key={product.id} className='border flex w-64'>
+                      <img
+                        width={70}
+                        height={70}
+                        src={product.imageUrl}
+                        alt=''
+                      />
+                      <div className='flex flex-col p-3'>
+                        <p className='text-sm'>{product.name}</p>
+                        <span className='text-sm font-bold mt-2'>
+                          {product.price} $
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className='text-l font-bold mt-5'>
+                  Total price:{' '}
+                  {order.products.reduce((sum, obj) => obj.price + sum, 0)} $
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <InfoPage
+          img={emptyOrders}
+          title='No orders!'
+          description='Please order!'
+        />
+      )}
+    </div>
+  );
+};
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,16 +1,11 @@
-import { InfoPage } from '../components/InfoPage/InfoPage';
-import emptyOrders from '../img/emptyOrders.png';
+import { OrdersItems } from '../components/Orders/OrdersItems';
 
 export const UserProfile = () => {
   return (
     <>
       <div className='px-10 py-3 flex-1'>
         <h3 className='text-2xl font-bold'>UserProfile</h3>
-        <InfoPage
-          img={emptyOrders}
-          title='No orders!'
-          description='Please order!'
-        />
+        <OrdersItems />
       </div>
     </>
   );
